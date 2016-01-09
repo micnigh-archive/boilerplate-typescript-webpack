d22fbd725c2ec5ff7cd93b839615f21e2749e9fa
Michael Nigh <mic.nigh@gmail.com>
2016-01-09 00:02:58 -0800

    Add more features and better caching

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,24 +4,21 @@ var distPath = isDev ? "./.tmp/development/" : "./.tmp/production/";
 var webpack = require("webpack");
 var WebpackNotifierPlugin = require("webpack-notifier");
 
-var libs = [
-  "underscore",
-  "es5-shim/es5-shim",
-  "es5-shim/es5-sham",
-];
-
-module.exports = [{
-  // devtool: isDev ? "eval" : "source-map",
-  //devtool: "source-map",
-  // debug: isDev,
-  cache: isDev,
-  devtool: "inline-eval-cheap-source-map",
+module.exports = {
+  cache: true,
+  devtool: "#cheap-module-source-map",
   entry: {
     app: isDev ? [
       "webpack-hot-middleware/client",
       "./client/src/app.ts",
     ] : [
       "./client/src/app.ts",
+    ],
+    lib: [
+      "underscore",
+      "es5-shim",
+      "es5-shim/es5-sham",
+      "regenerator/runtime",
     ],
   },
   devServer: {
@@ -34,43 +31,39 @@ module.exports = [{
     filename: "[name].js",
     chunkFilename: "[chunkhash].js"
   },
-  externals: libs,
   module: {
     loaders: [{
       test: /\.ts(x?)$/,
-      exclude: /node_modules/,
+      exclude: [
+        /node_modules/,
+      ],
       loaders: [
-        "babel-loader?presets[]=es2015",
-        "ts-loader",
+        "babel-loader?presets[]=es2015&cacheDirectory",
+        "ts-loader?transpileOnly=true",
+      ],
+    }, {
+      test: /\.scss$/,
+      loaders: [
+        "style/useable",
+        "css",
+        "autoprefixer-loader",
+        "sass"
       ],
     }],
-    noParse: [
-
-    ],
+    sassLoader: {
+      includePaths: [],
+    },
   },
   plugins: isDev ? [
+    new webpack.optimize.CommonsChunkPlugin({ name: "lib", filename: "lib.js" }),
     new webpack.HotModuleReplacementPlugin(),
     new WebpackNotifierPlugin({ title: "Webpack build", excludeWarnings: true })
   ] : [
+    new webpack.optimize.CommonsChunkPlugin({ name: "lib", filename: "lib.js" }),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin()
   ],
   resolve: {
     extensions: [ "", ".webpack.js", ".web.js", ".ts", ".tsx", ".js" ]
   },
-}, {
-  entry: {
-    lib: libs,
-  },
-  output: {
-    path: distPath,
-    filename: "[name].js",
-    chunkFilename: "[chunkhash].js"
-  },
-  plugins: isDev ? [
-    new WebpackNotifierPlugin({ title: "Webpack build", excludeWarnings: true })
-  ] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin()
-  ],
-}];
+};
